66f76ecee6890979421eb9cccd46c48fa84a0906
fengrongcheng <frcwindy@gmail.com>
2019-10-29 17:19:00 +0800

    add : frclog mudule

---
+++ frclog/src/main/java/com/rong/cheng/frclog/FrcLog.kt
@@ -0,0 +1,15 @@
+package com.rong.cheng.frclog
+
+import android.util.Log
+
+/**
+ * @author  frc  create on  2019-10-29
+ *
+ */
+class FrcLog {
+    companion object {
+        public fun msgE(msg: String) {
+            Log.e("Frc", msg)
+        }
+    }
+}
